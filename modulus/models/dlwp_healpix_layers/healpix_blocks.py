2230bed1271cca4bbea579aeba097c8ed932c60e
Bowen Liu <bowenliu@uw.edu>
2025-03-09 22:46:58 -0700

    new convNext block

--- modulus/models/dlwp_healpix_layers/healpix_blocks.py
+++ modulus/models/dlwp_healpix_layers/healpix_blocks.py
@@ -20,7 +20,7 @@ import torch
 import torch as th
 
 from .healpix_layers import HEALPixLayer
-
+from timm.models.layers import DropPath
 #
 # RECURRENT BLOCKS
 #
@@ -565,6 +565,7 @@ class SymmetricConvNeXtBlock(th.nn.Module):
         activation: th.nn.Module = None,
         enable_nhwc: bool = False,
         enable_healpixpad: bool = False,
+        drop_path: float = 0.2,
     ):
         """
         Parameters
@@ -589,6 +590,7 @@ class SymmetricConvNeXtBlock(th.nn.Module):
             Enable nhwc format, passed to wrapper
         enable_healpixpad: bool, optional
             If HEALPixPadding should be enabled, passed to wrapper
+        drop_path (float): Stochastic depth rate. Default: 0.2
         """
         super().__init__()
 
@@ -606,20 +608,30 @@ class SymmetricConvNeXtBlock(th.nn.Module):
 
         # 1st ConvNeXt block, the output of this one remains internal
         convblock = []
-        # 3x3 convolution establishing latent channels channels
+        # 7x7 convolution establishing latent channels channels
         convblock.append(
             geometry_layer(
                 layer=torch.nn.Conv2d,
                 in_channels=in_channels,
                 out_channels=int(latent_channels),
-                kernel_size=kernel_size,
+                kernel_size=7,
                 dilation=dilation,
                 enable_nhwc=enable_nhwc,
                 enable_healpixpad=enable_healpixpad,
             )
         )
-        if activation is not None:
-            convblock.append(activation)
+        # Apply LayerNorm
+        convblock.append(LayerNorm(int(latent_channels), eps=1e-6))
+        # if batch_norm:
+        #     convblock.append(
+        #         th.nn.BatchNorm2d(int(latent_channels), track_running_stats=False, affine=False)
+        #     )
+        # if activation is not None:
+        #     convblock.append(activation)
+        # # Apply Dropout 
+        # if dropout: 
+        #     convblock.append(th.nn.Dropout2d(p=dropout))
+
         # 1x1 convolution establishing increased channels
         convblock.append(
             geometry_layer(
@@ -632,37 +644,42 @@ class SymmetricConvNeXtBlock(th.nn.Module):
                 enable_healpixpad=enable_healpixpad,
             )
         )
-        if activation is not None:
-            convblock.append(activation)
+        # if activation is not None:
+        convblock.append(activation)
+        # apply Global Response Normalization
+        convblock.append(GRN(int(latent_channels * upscale_factor)))
+
         # 1x1 convolution returning to latent channels
         convblock.append(
             geometry_layer(
                 layer=torch.nn.Conv2d,
                 in_channels=int(latent_channels * upscale_factor),
-                out_channels=int(latent_channels),
+                out_channels=out_channels,
                 kernel_size=1,
                 dilation=dilation,
                 enable_nhwc=enable_nhwc,
                 enable_healpixpad=enable_healpixpad,
             )
         )
-        if activation is not None:
-            convblock.append(activation)
-        # 3x3 convolution from latent channels to latent channels
-        convblock.append(
-            geometry_layer(
-                layer=torch.nn.Conv2d,
-                in_channels=int(latent_channels),
-                out_channels=out_channels,  # int(latent_channels),
-                kernel_size=kernel_size,
-                dilation=dilation,
-                enable_nhwc=enable_nhwc,
-                enable_healpixpad=enable_healpixpad,
-            )
-        )
-        if activation is not None:
-            convblock.append(activation)
+        # if activation is not None:
+        #     convblock.append(activation)
+        # # 3x3 convolution from latent channels to latent channels
+        # convblock.append(
+        #     geometry_layer(
+        #         layer=torch.nn.Conv2d,
+        #         in_channels=int(latent_channels),
+        #         out_channels=out_channels,  # int(latent_channels),
+        #         kernel_size=kernel_size,
+        #         dilation=dilation,
+        #         enable_nhwc=enable_nhwc,
+        #         enable_healpixpad=enable_healpixpad,
+        #     )
+        # )
+        # if activation is not None:
+        #     convblock.append(activation)
         self.convblock = th.nn.Sequential(*convblock)
+
+        self.drop_path = DropPath(drop_path) if drop_path > 0. else th.nn.Identity()
 
     def forward(self, x):
         """Forward pass of the SymmetricConvNextBlock
@@ -678,8 +695,50 @@ class SymmetricConvNeXtBlock(th.nn.Module):
             result of the forward pass
         """
         # residual connection with reshaped inpute and output of conv block
-        return self.skip_module(x) + self.convblock(x)
+        return self.skip_module(x) + self.drop_path(self.convblock(x))
 
+
+class LayerNorm(th.nn.Module):
+    """ 
+    LayerNorm that supports two data formats: channels_last or channels_first (default). 
+    The ordering of the dimensions in the inputs. channels_last corresponds to inputs with 
+    shape (batch_size, height, width, channels) while channels_first corresponds to inputs 
+    with shape (batch_size, channels, height, width).
+    Code modified from
+    https://github.com/facebookresearch/ConvNeXt/blob/main/models/convnext.py
+    """
+    def __init__(self, normalized_shape, eps=1e-6, data_format="channels_first"):
+        super().__init__()
+        self.weight = th.nn.Parameter(torch.ones(normalized_shape))
+        self.bias = th.nn.Parameter(torch.zeros(normalized_shape))
+        self.eps = eps
+        self.data_format = data_format
+        if self.data_format not in ["channels_last", "channels_first"]:
+            raise NotImplementedError 
+        self.normalized_shape = (normalized_shape, )
+    
+    def forward(self, x):
+        if self.data_format == "channels_last":
+            return th.nn.functional.layer_norm(x, self.normalized_shape, self.weight, self.bias, self.eps)
+        elif self.data_format == "channels_first":
+            u = x.mean(1, keepdim=True)
+            s = (x - u).pow(2).mean(1, keepdim=True)
+            x = (x - u) / torch.sqrt(s + self.eps)
+            x = self.weight[:, None, None] * x + self.bias[:, None, None]
+            return x
+
+class GRN(th.nn.Module):
+    """ GRN (Global Response Normalization) layer
+    """
+    def __init__(self, dim):
+        super().__init__()
+        self.gamma = th.nn.Parameter(torch.zeros(1, 1, 1, dim))
+        self.beta = th.nn.Parameter(torch.zeros(1, 1, 1, dim))
+
+    def forward(self, x):
+        Gx = torch.norm(x, p=2, dim=(2,3), keepdim=True)
+        Nx = Gx / (Gx.mean(dim=1, keepdim=True) + 1e-6)
+        return self.gamma * (x * Nx) + self.beta + x
 
 #
 # DOWNSAMPLING BLOCKS
@@ -781,6 +840,32 @@ class AvgPool(th.nn.Module):
         """
         return self.avgpool(x)
 
+class downsample_conv_block(th.nn.Module):
+    """use conv2d(kernel=2, stride=2) to downsample the input tensor
+    -> layernorm -> conv2d
+    """
+    def __init__(
+            self,
+            geometry_layer: th.nn.Module = HEALPixLayer,
+            in_channels: int = 1,
+            out_channels: int = 1,
+            enable_nhwc: bool = False,
+            enable_healpixpad: bool = False,
+    ):
+        super().__init__()
+        self.layernorm = LayerNorm(in_channels)
+        self.conv2d = geometry_layer(
+            layer=torch.nn.Conv2d,
+            in_channels=in_channels,
+            out_channels=out_channels,
+            kernel_size=2,
+            stride=2,
+            enable_nhwc=enable_nhwc,
+            enable_healpixpad=enable_healpixpad,
+        )
+    def forward(self, x):
+        x = self.layernorm(x)
+        return self.conv2d(x)
 
 #
 # UPSAMPLING BLOCKS
--- modulus/models/dlwp_healpix_layers/healpix_encoder.py
+++ modulus/models/dlwp_healpix_layers/healpix_encoder.py
@@ -75,6 +75,8 @@ class UNetEncoder(th.nn.Module):
                 modules.append(
                     instantiate(
                         config=down_sampling_block,
+                        in_channels=old_channels,
+                        out_channels=old_channels,
                         enable_nhwc=enable_nhwc,
                         enable_healpixpad=enable_healpixpad,
                     )
